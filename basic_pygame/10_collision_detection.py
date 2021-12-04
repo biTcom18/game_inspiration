41aa5cef36affa4ba8dff4df5b73eb7895e45ede
unknown <C:\Users\nabei\AppData\Roaming\The Bat!>
2021-12-04 19:36:32 +1000

    Collision Detection

--- basic_pygame/10_collision_detection.py
+++ basic_pygame/10_collision_detection.py
@@ -1,13 +1,14 @@
 import pygame
+import random
 
-# Initialize pygame
+# Inialize pygame
 pygame.init()
 
 # Create a display surface
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 300
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Restricted Movement")
+pygame.display.set_caption("Collision Detection")
 
 # Set FPS and clock
 FPS = 30
@@ -17,8 +18,13 @@ clock = pygame.time.Clock()
 VELOCITY = 5
 
 # Load images
-dragon_image = pygame.image.load('dragon_left.png')
+dragon_image = pygame.image.load("dragon_left.png")
 dragon_rect = dragon_image.get_rect()
+dragon_rect.topleft = (525, 25)
+
+coin_image = pygame.image.load("coin.png")
+coin_rect = coin_image.get_rect()
+coin_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
 
 
 # The main loop
@@ -27,37 +33,44 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+            
     # Get a list of all keys currently pressed down
     keys = pygame.key.get_pressed()
-    print(keys)
     
-    # Move the dragon continiously
-    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and dragon_rect.left > 0:
+    if keys[pygame.K_LEFT] and dragon_rect.left > 0:
         dragon_rect.x -= VELOCITY
     if keys[pygame.K_RIGHT] and dragon_rect.right < WINDOW_WIDTH:
         dragon_rect.x += VELOCITY
     if keys[pygame.K_UP] and dragon_rect.top > 0:
         dragon_rect.y -= VELOCITY
     if keys[pygame.K_DOWN] and dragon_rect.bottom < WINDOW_HEIGHT:
-        dragon_rect.y += VELOCITY 
+        dragon_rect.y += VELOCITY
     
-    # Fill the display
+    
+    # Fill display surface
     display_surface.fill((0, 0, 0))
+    
+    # Draw rectanges to represent the rects of each object
+    pygame.draw.rect(display_surface, (0,255,0), dragon_rect, 1)
+    pygame.draw.rect(display_surface,(255,255,0), coin_rect, 1)
+    
+    # Check for collision between two rects
+    if dragon_rect.colliderect(coin_rect):
+        print("HIT")
+        coin_rect.x = random.randint(0, WINDOW_WIDTH - 32)
+        coin_rect.y = random.randint(0, WINDOW_HEIGHT - 32)
     
     # Blit assets
     display_surface.blit(dragon_image, dragon_rect)
-    
+    display_surface.blit(coin_image, coin_rect)
     
     # Update display
     pygame.display.update()
+    
+    # Tick the clock
     clock.tick(FPS)
-    
-    
+                
+            
+            
 # End the game
 pygame.quit()
-
-
-
-
-
