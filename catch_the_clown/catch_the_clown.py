fd55c0c97b5d95a4ed14b9367f78647d761ed008
unknown <C:\Users\nabei\AppData\Roaming\The Bat!>
2021-12-06 18:27:57 +1000

    Catch for the Clown Setup

--- catch_the_clown/catch_the_clown.py
+++ catch_the_clown/catch_the_clown.py
@@ -46,12 +46,28 @@ lives_text = font.render("Lives: " + str(player_lives), True, YELLOW)
 lives_rect = lives_text.get_rect()
 lives_rect.topright = (WINDOW_WIDTH - 50, 50)
 
+game_over_text = font.render("GAMEOVER", True, YELLOW)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HIGHT//2)
+
+continue_text = font.render("Click anywhere to play again", True, YELLOW, BLUE)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HIGHT//2 + 64)
 
 # Set sound and music
+click_sound = pygame.mixer.Sound("click_sound.wav")
+miss_sound = pygame.mixer.Sound("miss_sound.wav")
+pygame.mixer.music.load("ctc_background_music.wav")
  
 # Set images
+background_image = pygame.image.load("background.png")
+background_rect = background_image.get_rect()
+background_rect.topleft = (0, 0)
 
 
+clown_image = pygame.image.load("clown.png")
+clown_rect = clown_image.get_rect()
+clown_rect.center = (WINDOW_WIDTH//2, WINDOW_HIGHT//2)
 
 # The main gaim loop
 running = True
@@ -60,6 +76,21 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+    # Blit the background
+    display_surface.blit(background_image, background_rect)
+    
+    # Blit HUD
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(score_text, score_rect)
+    display_surface.blit(lives_text, lives_rect)
+    
+    # Blit assets
+    display_surface.blit(clown_image, clown_rect)
+    
+    # Update the display and tick clock
+    pygame.display.update()
+    clock.tick(FPS)
             
 # End the game
 pygame.quit()
