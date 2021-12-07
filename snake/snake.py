447bbfb57f76cf0bccabca5caf77c3182e556af7
unknown <C:\Users\nabei\AppData\Roaming\The Bat!>
2021-12-07 14:21:24 +1000

    Setup finished

--- snake/snake.py
+++ snake/snake.py
@@ -11,7 +11,7 @@ pygame.display.set_caption("~~Snake~~")
 
 # Set FPS and clock
 FPS = 20
-clock = pygame.display.Clock()
+clock = pygame.time.Clock()
 
 # Set game values
 SNAKE_SIZE = 20
@@ -37,11 +37,36 @@ font = pygame.font.SysFont("gabriola",  48)
 
 # Set text
 title_text = font.render("~~Snake~~", True, GREEN, DARKRED)
+title_rect = title_text.get_rect()
+title_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+
+score_text = font.render("Score: " + str(score), True, GREEN, DARKRED)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 10)
+
+game_over_text = font.render("GAMEOVER", True, RED, DARKGREEN)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_WIDTH//2)
+
+
+
+continue_text = font.render('Press any key to play again', True, RED, DARKGREEN)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 64)
 
 
 # Set sound and music
+pick_up_sound = pygame.mixer.Sound("pick_up.wav")
 
-# Set images
+# Set images ( in this case, use simple rects ... so just create their coordinates top-left x,top-left y, width, height)
+apple_coord = (500, 500, SNAKE_SIZE, SNAKE_SIZE) 
+apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
+
+head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+head_rect = pygame.draw.rect(display_surface, RED, head_coord)
+
+body_coord = []
+
 
 # The main game loop
 running = True
@@ -50,8 +75,22 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
-            
+    display_surface.fill(WHITE)
+    # Blit HUD
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(score_text, score_rect)
+    
+                
+    # Blit assets and still need the body
+    pygame.draw.rect(display_surface, GREEN, head_coord)
+    pygame.draw.rect(display_surface, RED, apple_coord)
+    
+    # Update display and tick clock
+    pygame.display.update()
+    clock.tick(FPS)
+    
+                
+                        
 # End the game
 pygame.quit()
 
