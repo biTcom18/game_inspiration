615fc1d494eedb520da61b2c4f58cf766a51f951
biTcom18 <nabei@bitcom.info>
2021-12-17 14:29:32 +1000

    Alien Class finished

--- space_invaders/space_invaders.py
+++ space_invaders/space_invaders.py
@@ -97,13 +97,32 @@ class Player(pygame.sprite.Sprite):
     
 class Alien(pygame.sprite.Sprite):
     """ A class to model an enemy alien """
-    def __init__(self):
+    def __init__(self, x, y, velocity, bullet_group):
         """ Initialize the alien """
-        pass
+        super().__init__()
+        self.image = pygame.image.load("alien.png")
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        
+        self.starting_x = x
+        self.starting_y = y
+        
+        self.direction = 1
+        self.velocity = velocity
+        self.bullet_group = bullet_group
+        
+        self.shoot_sound = pygame.mixer.Sound("alien_fire.wav")
+        
     
     def update(self):
         """ Update the alien """
-        pass
+        self.rect.x += self.direction*self.velocity
+        
+        # Randomly fire a bullet
+        if random.randint(0, 1000) > 999 and len(self.bullet_group) < 3:
+            self.shoot_sound.play()
+            self.fire()
+            
     
     def fire(self):
         """ Fire a bullet """
