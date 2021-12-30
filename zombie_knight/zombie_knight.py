c06dbcabc9ed4ea05c99029a0150a9c8aabc59a7
biTcom18 <nabei@bitcom.info>
2021-12-30 23:03:53 +1000

    Zombie Knight Player Class 1

--- zombie_knight/zombie_knight.py
+++ zombie_knight/zombie_knight.py
@@ -144,10 +144,117 @@ class Tile(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     """ A class the user can control """
     
-    def __ini__(self):
+    def __ini__(self, x, y, platform_group, portal_group, bullet_group):
         """ Initialize the player """
-        pass
+        super.__init__()
+        
+        # Set constant variables
+        self.HORISONTAL_ACCELERATION = 2
+        self.HORISONTAL_FRICTION = 0.15
+        self.VERTICAL_ACCELERATION = 0.8 #Gravity
+        self.VERTICAL_JUMP_SPEED = 18 #Determines how high the player can jump
+        self.STARTING_HEALTH = 100
+        
+        # Animation frames
+        self.move_right_sprites = []
+        self.move_left_sprites = []
+        self.idle_right_sprites = []
+        self.idle_left_sprites = []
+        self.jump_right_sprites = []
+        self.jump_left_sprites = []
+        self.attack_right_sprites = []
+        self.attack_left_sprites = []
+        
+        # Moving
+        self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/run/Run (1).png"), (64,64))) 
+        self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/run/Run (2).png"), (64,64)))
+        self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/run/Run (3).png"), (64,64)))
+        self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/run/Run (4).png"), (64,64)))
+        self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/run/Run (5).png"), (64,64)))
+        self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/run/Run (6).png"), (64,64)))
+        self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/run/Run (7).png"), (64,64)))
+        self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/run/Run (8).png"), (64,64)))
+        self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/run/Run (9).png"), (64,64)))
+        self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/run/Run (10).png"), (64,64)))
+        for sprite in self.move_right_sprites:
+            self.move_left_sprites.append(pygame.transform.flip(sprite, True, False))
+            
+        # Idling
+        self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/idle/Idle (1).png"), (64,64))) 
+        self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/idle/Idle (2).png"), (64,64)))
+        self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/idle/Idle (3).png"), (64,64)))
+        self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/idle/Idle (4).png"), (64,64)))
+        self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/idle/Idle (5).png"), (64,64)))
+        self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/idle/Idle (6).png"), (64,64)))
+        self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/idle/Idle (7).png"), (64,64)))
+        self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/idle/Idle (8).png"), (64,64)))
+        self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/idle/Idle (9).png"), (64,64)))
+        self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/idle/Idle (10).png"), (64,64)))
+        for sprite in self.idle_right_sprites:
+            self.idle_left_sprites.append(pygame.transform.flip(sprite, True, False))
+        
+        # Jumping
+        self.jump_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/jump/Jump (1).png"), (64,64))) 
+        self.jump_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/jump/Jump (2).png"), (64,64)))
+        self.jump_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/jump/Jump (3).png"), (64,64)))
+        self.jump_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/jump/Jump (4).png"), (64,64)))
+        self.jump_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/jump/Jump (5).png"), (64,64)))
+        self.jump_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/jump/Jump (6).png"), (64,64)))
+        self.jump_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/jump/Jump (7).png"), (64,64)))
+        self.jump_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/jump/Jump (8).png"), (64,64)))
+        self.jump_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/jump/Jump (9).png"), (64,64)))
+        self.jump_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/jump/Jump (10).png"), (64,64)))
+        for sprite in self.jump_right_sprites:
+            self.jump_left_sprites.append(pygame.transform.flip(sprite, True, False))
+        
+        # Jumping
+        self.attack_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/attack/Attack (1).png"), (64,64))) 
+        self.attack_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/attack/Attack (2).png"), (64,64)))
+        self.attack_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/attack/Attack (3).png"), (64,64)))
+        self.attack_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/attack/Attack (4).png"), (64,64)))
+        self.attack_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/attack/Attack (5).png"), (64,64)))
+        self.attack_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/attack/Attack (6).png"), (64,64)))
+        self.attack_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/attack/Attack (7).png"), (64,64)))
+        self.attack_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/attack/Attack (8).png"), (64,64)))
+        self.attack_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/attack/Attack (9).png"), (64,64)))
+        self.attack_right_sprites.append(pygame.transform.scale(pygame.image.load("zombie_knight/images/player/attack/Attack (10).png"), (64,64)))
+        for sprite in self.attack_right_sprites:
+            self.attack_left_sprites.append(pygame.transform.flip(sprite, True, False))
+        
+        # Load image and get rect
+        self.current_sprite = 0
+        self.image = self.idle_right_sprites[self.current_sprite]
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = (x, y)
+        
+        # Attach sprite groups
+        self.platform_group = platform_group
+        self.portal_group = portal_group
+        self.bullet_group = bullet_group
+        
+        # Animation booleans
+        self.animate_jump = False
+        self.animate_fire = False
+        
+        # Load sounds
+        self.jump_sound = pygame.mixer.Sound("zombie_knight/sounds/jump_sound.wav")
+        self.slash_sound = pygame.mixer.Sound("zombie_knight/sounds/slash_sound.wav")
+        self.portal_sound = pygame.mixer.Sound("zombie_knight/sounds/portal_sound.wav")
+        self.hit_sound = pygame.mixer.Sound("zombie_knight/player_hit.wav")
+        
+        # Kinematics vectors
+        self.position = vector(x, y)
+        self.velocity = vector(0, 0)
+        self.acceleration = vector(0, self.VERTICAL_ACCELERATION)
+        
+        # Set initial player values
+        self.health = self.STARTING_HEALTH
+        self.starting_x = x
+        self.starting_y = y
     
+            
+        
+        
     def update(self):
         """ Update the player """
         pass
