2a0989360d8229daad86d87452b431fa7148a9a9
unknown <C:\Users\nabei\AppData\Roaming\The Bat!>
2022-01-15 16:04:53 +1000

    Ball Initialization and Ball Movement

--- bricks/bricks.py
+++ bricks/bricks.py
@@ -17,19 +17,28 @@ clock = pygame.time.Clock()
 # Set colors 
 BLACK = (0, 0, 0)
 
-# Set game values
+# Settings for bat
 player_Y = 540
 mouse_x = 0
 mouse_y = player_Y
+
+# Settings for ball
+ball_start_Y = 200
+ball_speed = 3
+ball_served = False
 
 # Bat initialization
 bat_image = pygame.image.load("bat.png")
 bat_rect = bat_image.get_rect()
 
+# Ball initialization
+ball_image = pygame.image.load("ball.png")
+ball_rect = ball_image.get_rect()
 
+b_X, b_Y = (24, ball_start_Y)
+s_X, s_Y = (ball_speed, ball_speed)
+ball_rect.topleft = (b_X, b_Y)
 
-
-# ball init
 # brick init
 
 
@@ -42,13 +51,41 @@ while running:
     
     # bat and ball draw
     display_surface.blit(bat_image, bat_rect)
+    display_surface.blit(ball_image, ball_rect)
     
     # all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        elif event.type == pygame.MOUSEMOTION:
+            mouse_x, mouse_y = event.pos
+            if (mouse_x < 800 - 55):
+                bat_rect.topleft = (mouse_x, player_Y)
+            else:
+                bat_rect.topleft = (800 - 55, player_Y)
+            
             
     # main game logic
+    b_X += s_X
+    b_Y += s_Y
+    ball_rect.topleft = (b_X, b_Y)
+    
+    if (b_Y <= 0):
+        b_Y = 0
+        s_Y *= -1
+        
+    if (b_Y >= 600-8):
+        b_Y = 600 - 8
+        s_Y *= -1
+    
+    if (b_X <= 0):
+        b_X = 0
+        s_X *= -1
+        
+    if (b_X >= 800-8):
+        b_X = 800 - 8
+        s_X *= -1
     
     # collision detection
     
